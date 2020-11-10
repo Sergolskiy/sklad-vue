58c0314d9672e37ec99174f2537545aa43af113f
ARTEM <girukartem@gmail.com>
2020-11-10 17:52:21 +0200

    registration module add

--- src/components/modules/AuthModule/AuthRoutes.js
+++ src/components/modules/AuthModule/AuthRoutes.js
@@ -2,22 +2,12 @@
 import {routerPaths} from '../../../router/routerPaths'
 
 const Login = () => import(/* webpackChunkName: "group-login" */ './components/Login/Login.vue')
-const SignUp = () => import(/* webpackChunkName: "group-signUp" */ './components/SignUp/SignUp.vue')
 const Forgot = () => import(/* webpackChunkName: "group-forgot" */ './components/Forgot/Forgot.vue')
 
 export const AuthRoutes = [
   {
     path: routerPaths.login,
     component: Login,
-    meta: {
-      layout: {
-        name: 'auth',
-      },
-    },
-  },
-  {
-    path: routerPaths.signUp,
-    component: SignUp,
     meta: {
       layout: {
         name: 'auth',
---
+++ src/components/modules/RegistrationModule/Registration.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <SignUp/>
+  </div>
+
+</template>
+
+<script>
+  import SignUp from './components/SignUp/SignUp.vue'
+
+  export default {
+    name: "Registration",
+
+    components: {
+      SignUp
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
